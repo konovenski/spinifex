3647fdd5de96a32549dc63f03287d1da26c82f38
Julian Sommer <115516918+juliansommer@users.noreply.github.com>
2026-09-15 11:57:57 +1000

    fix: theme button animation

--- spinifex/services/spinifexui/frontend/src/components/theme-switch.tsx
+++ spinifex/services/spinifexui/frontend/src/components/theme-switch.tsx
@@ -19,13 +19,13 @@ export function ThemeSwitch() {
 
   return (
     <Button
-      className="rounded-full"
+      className="rounded-full transition-none hover:bg-transparent dark:hover:bg-transparent"
       onClick={toggleTheme}
       size="icon-sm"
       variant="ghost"
     >
-      <Sun className="size-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-      <Moon className="absolute size-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      <Sun className="size-[1.2rem] scale-100 rotate-0 transition-transform dark:scale-0 dark:-rotate-90" />
+      <Moon className="absolute size-[1.2rem] scale-0 rotate-90 transition-transform dark:scale-100 dark:rotate-0" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   )
